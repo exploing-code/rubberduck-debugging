8cabeb3c666026ba40f3b839714b057e1ecf379f
Minimoocodes <jaemin.shim14@gmail.com>
2024-05-24 17:00:43 +0200

    hero text animation added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,33 +1,79 @@
-import React from 'react';
+import React from "react";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
-import { ducks } from '../../data';
-import { myContext } from '../components/ContextProvider.jsx';
+import { ducks } from "../../data";
+import { myContext } from "../components/ContextProvider.jsx";
 
 function Hero() {
   const { activeDuck, setActiveDuck } = myContext();
+  const ref = useRef();
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({});
+      tl.fromTo(
+        ".reveal",
+        { opacity: 0 },
+        {
+          opacity: 1,
+          ease: "elastic",
+          stagger: 0.1,
+          duration: 1,
+        }
+      );
+      tl.fromTo(
+        ".reveal2",
+        { opacity: 0 },
+        {
+          opacity: 1,
+          ease: "elastic",
+          stagger: 0.1,
+          duration: 1,
+        }
+      );
+      tl.resume();
+    },
+    { scope: ref }
+  );
+
+  const string = "RUBBER DUCK";
+  const string2 = "DEBUGGING";
 
   return (
     <section
       className={`h-[300vh] w-[100vw] flex justify-center pt-[2rem]'
         `}
-      id='s1'
+      id="s1"
     >
-      <div className='relative  flex flex-col items-center  text-[14rem]'>
+      <div
+        ref={ref}
+        className="h-screen relative flex flex-col items-center justify-between text-[14rem]"
+      >
         <h1
-          className='relative'
+          className="mt-3"
           style={{
             color: ducks[activeDuck].secondaryClr,
           }}
         >
-          RUBBER DUCK
+          {string.split("").map((item, index) => (
+            <span className="reveal" key={index}>
+              {item}
+            </span>
+          ))}
         </h1>
         <h1
-          className='absolute z-10 top-0 translate-y-[35rem] text-[17rem]'
+          className="z-10 text-[17rem]"
           style={{
             color: ducks[activeDuck].secondaryClr,
           }}
         >
-          DEBUGGING
+          {string2.split("").map((item, index) => (
+            <span className="reveal2" key={index}>
+              {item}
+            </span>
+          ))}
         </h1>
       </div>
     </section>
