124a608a9508ff53f82d1764138790b11e1a2ca0
Alekssthlm <114854430+Alekssthlm@users.noreply.github.com>
2024-05-29 18:40:19 +0200

    Completed the development of the loading screens

--- src/sections/Conclusion.jsx
+++ src/sections/Conclusion.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react"
 
 function Conclusion() {
   return (
     <section
-      id='s7'
-      className=' flex-col flex pt-[2rem] justify-between bg-blue-400'
+      id="s7"
+      className=" flex-col flex pt-[2rem] justify-between bg-blue-400"
     >
-      <button className='z-50 bg-orange-500 max-w-[10rem] p-2 rounded-lg text-white font-bold text-xl'>
+      <button className="z-10 bg-orange-500 max-w-[10rem] p-2 rounded-lg text-white font-bold text-xl">
         Click For Loader
       </button>
-      <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-[10rem]'>LAST SECTION</h1>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-[10rem]">LAST SECTION</h1>
       </div>
     </section>
-  );
+  )
 }
 
-export default Conclusion;
+export default Conclusion
