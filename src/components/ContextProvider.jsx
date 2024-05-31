b7f8d0354ae34f2b49129893c672c51774d0e3a5
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-05-31 13:12:58 +0200

    play song on click

--- src/components/ContextProvider.jsx
+++ src/components/ContextProvider.jsx
@@ -4,26 +4,29 @@ import React, { createContext, useState } from "react";
 export const Context = createContext(null);
 
 export const ContextProvider = ({ children }) => {
-  const [activeDuck, setActiveDuck] = useState(0);
-  const [pressedButton, setPressedButton] = useState(false);
-  const [partyOn, setPartyOn] = useState(false);
+	const [activeDuck, setActiveDuck] = useState(0);
+	const [pressedButton, setPressedButton] = useState(false);
+	const [partyOn, setPartyOn] = useState(false);
+	const [renderS2Loading, setRenderS2Loading] = useState(false);
 
-  const value = {
-    activeDuck,
-    setActiveDuck,
-    pressedButton,
-    setPressedButton,
-    partyOn,
-    setPartyOn,
-  };
+	const value = {
+		activeDuck,
+		setActiveDuck,
+		pressedButton,
+		setPressedButton,
+		partyOn,
+		setPartyOn,
+		renderS2Loading,
+		setRenderS2Loading,
+	};
 
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+	return <Context.Provider value={value}>{children}</Context.Provider>;
 };
 
 export const myContext = () => {
-  const context = React.useContext(Context);
-  if (!context) {
-    throw new Error("useMyContext must be used within a ContextProvider");
-  }
-  return context;
+	const context = React.useContext(Context);
+	if (!context) {
+		throw new Error("useMyContext must be used within a ContextProvider");
+	}
+	return context;
 };
--- src/sections/AudioVisualizer.jsx
+++ src/sections/AudioVisualizer.jsx
@@ -3,59 +3,33 @@ import P from "../components/P";
 import AudioVisualizerWave from "../components/AudioVizualizerWave";
 import { myContext } from "../components/ContextProvider";
 
-export default function AudioVisualizer({ setRenderS2Loading }) {
-  const songRef = useRef(null);
+export default function AudioVisualizer() {
+	const songRef = useRef(null);
 
-  const { setPartyOn } = myContext();
+	const { setPartyOn, setRenderS2Loading } = myContext();
 
-  function toggleSongPlaying(e) {
-    if (e.target.checked) {
-      songRef.current.currentTime = 32.9;
-      songRef.current.play();
-    } else {
-      songRef.current.pause();
-    }
-  }
+	function handleClickNo() {
+		setPartyOn(true);
+    songRef.current.currentTime = 32.9;
+    songRef.current.play();
+	}
+	function handleClickYes() {
+		setRenderS2Loading(true);
+	}
 
-  function handleOnClick() {
-    console.log("click");
-  }
+	return (
+		<section className=" w-screen relative flex items-center justify-center z-10">
+			<div className="flex gap-6 absolute top-0 ">
+				<button onClick={handleClickNo} className="bg-yellow-400 py-4 px-6 rounded-md transition-all duration-200 hover:scale-110 hover:bg-yellow-200">
+					NO
+				</button>
+				<button onClick={handleClickYes} className="bg-yellow-400 py-4 px-6 rounded-md transition-all duration-200 hover:scale-110 hover:bg-yellow-200">
+					YES
+				</button>
+			</div>
 
-  function handleClickNo() {
-    setPartyOn(true);
-  }
-
-  return (
-    <section className=" w-screen relative flex items-center justify-center z-10">
-      <div className="flex gap-6 absolute top-0 ">
-        <button
-          onClick={handleClickNo}
-          className="bg-yellow-400 py-4 px-6 rounded-md transition-all duration-200 hover:scale-110 hover:bg-yellow-200"
-        >
-          NO
-        </button>
-        <button
-          onClick={setRenderS2Loading}
-          className="bg-yellow-400 py-4 px-6 rounded-md transition-all duration-200 hover:scale-110 hover:bg-yellow-200"
-        >
-          YES
-        </button>
-      </div>
-      <button onClick={handleOnClick}>test</button>
-
-      <div className=" uppercase px-4 rounded-[1rem] text-xl m-2 items-center flex border-black border-[1px] absolute top-0 left-0 ">
-        <label htmlFor="partySongToggle">
-          <P>play party song </P>
-        </label>
-        <input
-          type="checkbox"
-          id="partySongToggle"
-          onChange={(e) => toggleSongPlaying(e)}
-        />
-      </div>
-
-      <audio ref={songRef} src="../sound-effects/Wobbly-duck.mp3"></audio>
-      <AudioVisualizerWave />
-    </section>
-  );
+			<audio ref={songRef} src="../sound-effects/Wobbly-duck.mp3"></audio>
+			<AudioVisualizerWave />
+		</section>
+	);
 }
