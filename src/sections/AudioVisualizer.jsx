15baf3c3da044733d0d639031ba9460a27364e88
marcusryden92 <marcus.ryden1992@gmail.com>
2024-06-03 17:01:26 +0200

    //

--- src/sections/AudioVisualizer.jsx
+++ src/sections/AudioVisualizer.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import P from '../components/P';
-import Visualizer from '../components/Vizualizer';
-import { myContext } from '../components/ContextProvider';
-import Button from '../components/Button';
-import { gsap } from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ducks } from '../../data';
+import React, { useEffect, useRef, useContext } from "react";
+import P from "../components/P";
+import Visualizer from "../components/Vizualizer";
+import { myContext } from "../components/ContextProvider";
+import Button from "../components/Button";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function AudioVisualizer() {
   const { setPartyOn, setRenderS2Loading, activeDuck } = myContext();
@@ -15,22 +17,8 @@ export default function AudioVisualizer() {
   const buttonsRef = useRef(null);
   const sectionRef = useRef(null);
 
-  // disable scroll when entering the section
-  //   useEffect(() => {
-  //     function handleScroll() {
-  //       if (sectionRef) {
-  //         let yValue = sectionRef.current.getBoundingClientRect().y;
-  //         if (yValue === 0) {
-  //           document.body.style.overflow = 'hidden';
-  //         }
-  //       }
-  //     }
-
-  //     window.addEventListener('scroll', handleScroll);
-  //     return () => {
-  //       window.removeEventListener('scroll', handleScroll);
-  //     };
-  //   }, [sectionRef]);
+  const title = useRef();
+  const titleString = "CHOOSE ANOTHER DUCK";
 
   // Text and buttons enter
   useGSAP(
@@ -38,8 +26,8 @@ export default function AudioVisualizer() {
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: sectionRef.current,
-          start: 'top center',
-          end: 'bottom center',
+          start: "top center",
+          end: "bottom center",
         },
       });
       tl.to(textRef.current, {
@@ -49,7 +37,7 @@ export default function AudioVisualizer() {
       });
       tl.to(buttonsRef.current.children, {
         translateY: 0,
-        ease: 'back.out',
+        ease: "back.out",
         stagger: 0.1,
         duration: 0.2,
       });
@@ -57,40 +45,70 @@ export default function AudioVisualizer() {
     { scope: sectionRef }
   );
 
+  useGSAP(() => {
+    if (title.current) {
+      console.log("Title children:", title.current.children); // Debugging step
+      gsap.fromTo(
+        title.current.children,
+        { y: 40 },
+        {
+          y: 0,
+          ease: "elastic",
+          stagger: 0.05,
+          duration: 2,
+          scrollTrigger: {
+            trigger: sectionRef.current,
+            start: "bottom bottom",
+          },
+        }
+      );
+    }
+  }, []);
+
   function handleClickNo() {
     setPartyOn(true);
     songRef.current.currentTime = 32.9;
     songRef.current.play().catch((err) => {
-      console.error('Error playing song:', err);
+      console.error("Error playing song:", err);
     });
     gsap.to(wrapperRef.current, {
       opacity: 0,
     });
   }
+
   function handleClickYes() {
     setRenderS2Loading(true);
-    document.body.style.overflow = 'auto';
+    document.body.style.overflow = "auto";
   }
 
   return (
     <section
       ref={sectionRef}
-      className=' w-screen relative flex items-center justify-center h-screen'
+      className="w-screen relative flex items-center justify-center h-screen"
     >
-      <div ref={wrapperRef} className=' absolute top-4 left-6 z-10 '>
-        <P style={'mb-2 ml-4'}>
+      <div ref={wrapperRef} className="absolute top-4 left-6 z-10">
+        <P style={"mb-2 ml-4"}>
           <span ref={textRef}></span>
         </P>
         <div
           ref={buttonsRef}
-          className='flex gap-4 *:-translate-y-[100%] overflow-hidden'
+          className="flex gap-4 *:-translate-y-[100%] overflow-hidden"
         >
-          <Button onClick={handleClickNo} text='no' />
-          <Button onClick={handleClickYes} text='yes' />
+          <Button onClick={handleClickNo} text="no" />
+          <Button onClick={handleClickYes} text="yes" />
         </div>
       </div>
-      <audio ref={songRef} src='../sound-effects/Wobbly-duck.mp3'></audio>
-
+      <audio ref={songRef} src="../sound-effects/Wobbly-duck.mp3"></audio>
+      <h1 className="m-0 text-black lg:text-[5rem] flex" ref={title}>
+        {titleString
+          .replace(/ /g, "\u00a0")
+          .split("")
+          .map((item, index) => (
+            <span className="reveal relative" key={index}>
+              {item}
+            </span>
+          ))}
+      </h1>
       <Visualizer />
     </section>
   );
