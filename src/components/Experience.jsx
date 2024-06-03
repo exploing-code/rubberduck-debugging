537fd4426846b8335cd0042ba7ef40ea83f62b8b
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-06-03 12:34:33 +0200

    fix: pin on all sections

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,213 +1,248 @@
 // three fiber
-import { Float, Environment, useGLTF } from "@react-three/drei"
-import { useLoader, useThree } from "@react-three/fiber"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
-import { EffectComposer, N8AO, SMAA } from "@react-three/postprocessing"
+import { Float, Environment, useGLTF } from '@react-three/drei';
+import { useLoader, useThree } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { EffectComposer, N8AO, SMAA } from '@react-three/postprocessing';
 
 // react
-import React, { useRef, Suspense, useEffect, useState } from "react"
+import React, { useRef, Suspense, useEffect, useState } from 'react';
 
 // gsap
-import { gsap } from "gsap"
+import { gsap } from 'gsap';
 
-import { useGSAP } from "@gsap/react"
-import { ScrollTrigger } from "gsap/all"
-gsap.registerPlugin(ScrollTrigger)
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
-import { myContext } from "./ContextProvider.jsx"
-import { ducks } from "../../data"
+import { myContext } from './ContextProvider.jsx';
+import { ducks } from '../../data';
 
 // ducks.forEach((duck) => useGLTF.preload(duck.path));
 
 export default function Experience() {
   ducks.forEach((duck) => {
-    useGLTF.preload(duck.path)
-    console.log(`Preloaded model at path: ${duck.path}`)
-  })
+    useGLTF.preload(duck.path);
+    // console.log(`Preloaded model at path: ${duck.path}`);
+  });
 
   const { activeDuck, setActiveDuck, pressedButton, setPressedButton } =
-    myContext()
+    myContext();
 
   //   const [activeDuckUrl, setActiveDuckUrl] = useState(ducks[0].path);
-  const model = useGLTF(ducks[activeDuck].path)
-  const modelRef = useRef()
+  const model = useGLTF(ducks[activeDuck].path);
+  const modelRef = useRef();
 
-  const { size } = useThree()
-  const modelScale = size.width > 768 ? [1, 1, 1] : [0.7, 0.7, 0.7]
+  const { size } = useThree();
+  const modelScale = size.width > 768 ? [1, 1, 1] : [0.7, 0.7, 0.7];
 
-  // Function to create GSAP animations
-  const createAnimation = (
-    target,
-    scrollTriggerOptions,
-    animationProperties
-  ) => {
-    gsap.to(target, {
-      scrollTrigger: scrollTriggerOptions,
-      ...animationProperties,
-    })
-  }
-
-  // Function to create common scrollTrigger options
-  const createScrollTrigger = (
-    trigger,
-    start,
-    end,
-    scrub = 1,
-    pin = false,
-    endTrigger = null
-  ) => ({
-    trigger,
-    start,
-    end,
-    scrub,
-    pin,
-    endTrigger,
-  })
-
-  // Function to create GSAP timelines
-  const createTimeline = (
-    target,
-    scrollTriggerOptions,
-    animationProperties
-  ) => {
-    let timeline = gsap.timeline({
-      scrollTrigger: scrollTriggerOptions,
-    })
-
-    timeline.to(target, animationProperties)
-
-    return timeline
-  }
-
-  let windowSize = window.innerWidth
-  let smallWindow = windowSize < 600
-  let mediumWindow = windowSize >= 600 && windowSize < 900
-  let largeWindow = windowSize >= 900
+  let windowSize = window.innerWidth;
+  let smallWindow = windowSize < 600;
+  let mediumWindow = windowSize >= 600 && windowSize < 900;
+  let largeWindow = windowSize >= 900;
 
   // Use GSAP
   useGSAP(() => {
     if (model) {
       // HERO SECTION
-      createAnimation(
-        modelRef.current.rotation,
-        createScrollTrigger("#s1", "top top", "bottom bottom", 1, false, "#s2"),
-        { y: modelRef.current.rotation.y + Math.PI * 2, x: 6.5 }
-      )
-      createAnimation(
-        modelRef.current.position,
-        createScrollTrigger("#s1", "top top", "bottom center", 1, false, "#s2"),
-        { z: -4, y: mediumWindow ? 1.5 : largeWindow ? 1.5 : 0 }
-      )
+      gsap.to(modelRef.current.rotation, {
+        scrollTrigger: {
+          trigger: '#s1',
+          start: 'top top',
+          end: 'bottom bottom',
+          scrub: 1,
+          pin: false,
+          endTrigger: '#s2',
+        },
+        y: modelRef.current.rotation.y + Math.PI * 2,
+        x: 6.5,
+      });
+
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s1',
+          start: 'top top',
+          end: 'bottom center',
+          scrub: 1,
+          pin: false,
+          endTrigger: '#s2',
+        },
+        z: -4,
+        y: mediumWindow ? 1.5 : largeWindow ? 1.5 : 0,
+      });
 
       // CHARACTER SELECTION SECTION
-      createAnimation(
-        modelRef.current.position,
-        createScrollTrigger("#s2", "top top", "bottom center", 1, true),
-        {}
-      )
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s2',
+          start: 'top top',
+          end: 'bottom center',
+          scrub: 1,
+          pin: true,
+        },
+      });
 
       // DESC SECTION 1
-      createAnimation(
-        modelRef.current.position,
-        createScrollTrigger("#s3", "top bottom", "center center", 1),
-        {
-          x: smallWindow // SM
-            ? windowSize / 300
-            : mediumWindow // MD
-            ? windowSize / 400
-            : largeWindow //LG
-            ? windowSize / 300
-            : windowSize / 200,
-        }
-      )
-      createAnimation(
-        modelRef.current.rotation,
-        createScrollTrigger("#s3", "top bottom", "bottom bottom", 1),
-        { x: 1.5, y: -0.5, z: smallWindow ? 0 : 0.5 }
-      )
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s3',
+          start: 'top top',
+          end: 'center center',
+          scrub: 1,
+          pin: true,
+          endTrigger: '#s4',
+        },
+      });
+
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s3',
+          start: 'top bottom',
+          end: 'center center',
+          scrub: 1,
+          pin: false,
+          endTrigger: 's4',
+        },
+        x: smallWindow // SM
+          ? windowSize / 300
+          : mediumWindow // MD
+          ? windowSize / 400
+          : largeWindow //LG
+          ? windowSize / 300
+          : windowSize / 200,
+      });
+
+      gsap.to(modelRef.current.rotation, {
+        scrollTrigger: {
+          trigger: '#s3',
+          start: 'top bottom',
+          end: 'bottom bottom',
+          scrub: 1,
+        },
+        x: 1.5,
+        y: -0.5,
+        z: smallWindow ? 0 : 0.5,
+      });
 
       // DESC SECTION 2
-      createTimeline(
-        modelRef.current.position,
-        createScrollTrigger("#s4", "top center", "bottom bottom", 1),
-        {
-          x: smallWindow ? -1.5 : mediumWindow ? -1 : largeWindow ? -2.4 : -2.5,
-          y: smallWindow
-            ? -0.8
-            : mediumWindow
-            ? -0.9
-            : largeWindow
-            ? -0.95
-            : -1,
-          z: smallWindow ? 0.5 : mediumWindow ? 1 : largeWindow ? 0.5 : 1.2,
-        }
-      )
-      createTimeline(
-        modelRef.current.rotation,
-        createScrollTrigger("#s4", "top center", "bottom bottom", 1),
-        {
-          y: smallWindow ? 0.5 : mediumWindow ? 0.5 : largeWindow ? 0.6 : 0.8,
-          x: 0,
-          z: 0,
-        }
-      )
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s4',
+          start: 'top top',
+          end: 'center center',
+          scrub: 1,
+          pin: true,
+          endTrigger: '#s5',
+        },
+      });
+
+      let timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '#s4',
+          start: 'top center',
+          end: 'bottom bottom',
+          scrub: 1,
+        },
+      });
+
+      timeline.to(modelRef.current.position, {
+        x: smallWindow ? -1.5 : mediumWindow ? -1 : largeWindow ? -2.4 : -2.5,
+        y: smallWindow ? -0.8 : mediumWindow ? -0.9 : largeWindow ? -0.95 : -1,
+        z: smallWindow ? 0.5 : mediumWindow ? 1 : largeWindow ? 0.5 : 1.2,
+      });
+
+      timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '#s4',
+          start: 'top center',
+          end: 'bottom bottom',
+          scrub: 1,
+          //   duration: 4,
+        },
+      });
+
+      timeline.to(modelRef.current.rotation, {
+        y: smallWindow ? 0.5 : mediumWindow ? 0.5 : largeWindow ? 0.6 : 0.8,
+        x: 0,
+        z: 0,
+        scrub: 1,
+      });
 
       // DESC SECTION 3
-      createTimeline(
-        modelRef.current.position,
-        createScrollTrigger("#s5", "top bottom", "bottom bottom", 1),
-        { x: 0, y: 0, z: -1 }
-      )
-      createTimeline(
-        modelRef.current.rotation,
-        createScrollTrigger("#s5", "top bottom", "bottom bottom", 1),
-        { y: 0, x: 0.2 }
-      )
-    }
-  }, [])
+      gsap.to(modelRef.current.position, {
+        scrollTrigger: {
+          trigger: '#s5',
+          start: 'top top',
+          end: 'bottom bottom',
+          scrub: 1,
+          pin: true,
+          endTrigger: '#s6',
+        },
+      });
 
-  const [isAnimating, setIsAnimating] = useState(false)
+      timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '#s5',
+          start: 'top bottom',
+          end: 'bottom bottom',
+          scrub: 1,
+        },
+      });
+
+      timeline.to(modelRef.current.position, { x: 0, y: 0, z: -1 });
+
+      timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: '#s5',
+          start: 'top bottom',
+          end: 'bottom bottom',
+          scrub: 1,
+        },
+      });
+
+      timeline.to(modelRef.current.rotation, { y: 0, x: 0.2 });
+    }
+  }, []);
+
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
     if (!pressedButton || isAnimating) {
-      return
+      return;
     }
 
-    setIsAnimating(true)
+    setIsAnimating(true);
 
     const tl = gsap.timeline({
       onComplete: () => setIsAnimating(false),
-    })
+    });
 
-    const direction = pressedButton === "right" ? 1 : -1
+    const direction = pressedButton === 'right' ? 1 : -1;
 
     tl.to(modelRef.current.rotation, {
       duration: 0.3,
-      y: "+=" + direction * Math.PI * 2,
-      ease: "Power4.easeIn",
+      y: '+=' + direction * Math.PI * 2,
+      ease: 'Power4.easeIn',
       onComplete: () => {
-        if (pressedButton === "right") {
-          setActiveDuck((prev) => (prev + 1) % ducks.length)
+        if (pressedButton === 'right') {
+          setActiveDuck((prev) => (prev + 1) % ducks.length);
         } else {
-          setActiveDuck((prev) => (prev - 1 < 0 ? ducks.length - 1 : prev - 1))
+          setActiveDuck((prev) => (prev - 1 < 0 ? ducks.length - 1 : prev - 1));
         }
 
         tl.to(modelRef.current.rotation, {
           duration: 1,
-          y: "+=" + direction * Math.PI * 2 * 2,
-          ease: "Power4.easeOut",
-        })
-        modelRef.current.rotation.set(0, 0, 0)
+          y: '+=' + direction * Math.PI * 2 * 2,
+          ease: 'Power4.easeOut',
+        });
+        modelRef.current.rotation.set(0, 0, 0);
       },
-    })
+    });
 
-    setPressedButton(null)
-  }, [pressedButton, isAnimating])
+    setPressedButton(null);
+  }, [pressedButton, isAnimating]);
 
   return (
     <>
-      {/* <Perf position="top-left" /> */}
-      {/* <OrbitControls makeDefault /> */}
       <perspectiveCamera position={[0, 0.5, 0]}>
         <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
         <ambientLight intensity={1.5} />
@@ -223,18 +258,17 @@ export default function Experience() {
           </Float>
         </Suspense>
       </perspectiveCamera>
-      <Environment files="/adamsbridge.hdr" />
+      <Environment files='/adamsbridge.hdr' />
       <EffectComposer disableNormalPass multisampling={0}>
         <N8AO
           halfRes
-          color="black"
+          color='black'
           aoRadius={2}
           intensity={1}
           aoSamples={6}
           denoiseSamples={4}
         />
-        {/* <SMAA /> */}
       </EffectComposer>
     </>
-  )
+  );
 }
--- src/sections/DescSectionThree.jsx
+++ src/sections/DescSectionThree.jsx
@@ -1,98 +1,98 @@
-import React from "react"
-import { useRef } from "react"
+import React from 'react';
+import { useRef } from 'react';
 
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { TextPlugin } from "gsap/TextPlugin"
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { TextPlugin } from 'gsap/TextPlugin';
 
-import { ducks } from "../../data"
-import { myContext } from "../components/ContextProvider.jsx"
-import P from "../components/P"
-import speechBubble from "../assets/speechBubble.svg"
+import { ducks } from '../../data';
+import { myContext } from '../components/ContextProvider.jsx';
+import P from '../components/P';
+import speechBubble from '../assets/speechBubble.svg';
 
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(TextPlugin)
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 function DescSectionThree() {
-  const { activeDuck } = myContext()
-  const refS3 = useRef()
-  const bubbleRef3 = useRef()
-  const sectionRef3 = useRef()
+  const { activeDuck } = myContext();
+  const refS3 = useRef();
+  const bubbleRef3 = useRef();
+  const sectionRef3 = useRef();
 
-  useGSAP(() => {
-    // Opacity animation for wrapper
-    gsap.fromTo(
-      bubbleRef3.current,
-      { opacity: 0 },
-      {
-        scrollTrigger: {
-          trigger: sectionRef3.current,
-          start: "center bottom",
-          end: "bottom bottom",
-          pin: true,
-          toggleActions: "play reverse play reverse",
-          markers: true,
-        },
-        opacity: 1,
-        duration: 0.8,
-      }
-    )
+  //   useGSAP(() => {
+  //     // Opacity animation for wrapper
+  //     gsap.fromTo(
+  //       bubbleRef3.current,
+  //       { opacity: 0 },
+  //       {
+  //         scrollTrigger: {
+  //           trigger: sectionRef3.current,
+  //           start: 'center bottom',
+  //           end: 'bottom bottom',
+  //           pin: true,
+  //           toggleActions: 'play reverse play reverse',
+  //           markers: true,
+  //         },
+  //         opacity: 1,
+  //         duration: 0.8,
+  //       }
+  //     );
 
-    // Animation for speech bubble and text
-    gsap.fromTo(
-      bubbleRef3.current,
-      { y: -5 },
-      {
-        scrollTrigger: {
-          trigger: refS3.current,
-          start: "top top",
-          end: "bottom bottom",
-        },
-        y: 5,
-        duration: 1,
-        repeat: -1,
-        ease: "power1.inOut",
-        yoyo: true,
-      }
-    )
+  //     // Animation for speech bubble and text
+  //     gsap.fromTo(
+  //       bubbleRef3.current,
+  //       { y: -5 },
+  //       {
+  //         scrollTrigger: {
+  //           trigger: refS3.current,
+  //           start: 'top top',
+  //           end: 'bottom bottom',
+  //         },
+  //         y: 5,
+  //         duration: 1,
+  //         repeat: -1,
+  //         ease: 'power1.inOut',
+  //         yoyo: true,
+  //       }
+  //     );
 
-    gsap.to(".revealSectionThree", {
-      scrollTrigger: {
-        trigger: refS3.current,
-        start: "top top",
-        end: "bottom bottom",
-      },
-      duration: 2,
-      text: "Alright, it's time. Let's start the text, share your code and I'll do my best!",
-    })
-  })
+  //     gsap.to('.revealSectionThree', {
+  //       scrollTrigger: {
+  //         trigger: refS3.current,
+  //         start: 'top top',
+  //         end: 'bottom bottom',
+  //       },
+  //       duration: 2,
+  //       text: "Alright, it's time. Let's start the text, share your code and I'll do my best!",
+  //     });
+  //   });
 
   // const textColor = "[${ducks[activeDuck].secondaryClr}]"
 
   return (
     <section
       ref={refS3}
-      className="h-[200vh] flex items-start flex-col justify-start"
-      id="s5"
+      className='h-[100vh] flex items-start flex-col justify-start'
+      id='s5'
     >
-      <div ref={sectionRef3} className="w-full relative h-full">
+      <div ref={sectionRef3} className='w-full relative h-full'>
         <div
           ref={bubbleRef3}
           style={{
             backgroundImage: `url(${speechBubble}`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "contain",
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
           }}
-          className="flex absolute top-0 left-[60%] flex-col px-10 justify-center h-[90vmin] md:h-[70vmin] lg:h-[40vmin] w-[90vmin] lg:w-[40vmin] text-sm md:text-md lg:text-lg"
+          className='flex absolute top-0 left-[60%] flex-col px-10 justify-center h-[90vmin] md:h-[70vmin] lg:h-[40vmin] w-[90vmin] lg:w-[40vmin] text-sm md:text-md lg:text-lg'
         >
           <P>
-            <span className="revealSectionThree text-amber-200 "></span>
+            <span className='revealSectionThree text-amber-200 '></span>
           </P>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default DescSectionThree
+export default DescSectionThree;
