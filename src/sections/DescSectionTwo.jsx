bec94272e917809b24e5d4f7fa1465fd156c0326
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-06-04 16:53:09 +0200

    Feat: fixed colors on the emphasis

--- src/sections/DescSectionTwo.jsx
+++ src/sections/DescSectionTwo.jsx
@@ -1,15 +1,13 @@
 import React, { useRef } from 'react';
 import emphasis from '../assets/emphasis.svg';
 import SpeechBubble from '../components/SpeeachBubble.jsx';
-import { myContext } from '../components/ContextProvider.jsx';
-import { ducks } from '../../data';
+
+import Emphasis from '../components/Emphasis.jsx';
 
 function DescSectionTwo() {
   const refS2 = useRef();
   const sectionRef = useRef();
   const bubbleRef = useRef();
-
-  const { activeDuck } = myContext();
 
   return (
     <section ref={refS2} id='s4'>
@@ -18,10 +16,8 @@ function DescSectionTwo() {
         className='h-[100lvh] flex items-center lg:items-start justify-around'
       >
         <div className='translate-y-[2rem] sm:-translate-y-[10rem] lg:translate-y-[6rem] lg:translate-x-[4rem]'>
-          <img
-            // style={{ fill: ducks[activeDuck].secondaryClr }}
+          <Emphasis
             className={`rotate-[20deg] sm:rotate-[12deg]  lg:-rotate-12`}
-            src={emphasis}
           />
         </div>
 
