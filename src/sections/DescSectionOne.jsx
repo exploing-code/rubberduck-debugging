7fbce9dccf429ffd5842e7a0a54c093acf2dac87
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-06-04 10:05:29 +0200

    relative

--- src/sections/DescSectionOne.jsx
+++ src/sections/DescSectionOne.jsx
@@ -13,8 +13,6 @@ function DescSectionOne() {
 	const { activeDuck } = myContext();
 	const refS1 = useRef();
 
-	const primaryClr = ducks[activeDuck].primaryClr;
-
 	useGSAP(
 		() => {
 			const tl = gsap.timeline({
@@ -24,15 +22,15 @@ function DescSectionOne() {
 					end: "bottom center",
 				},
 			});
-			tl.to(".revealSectionOne1", {
+			tl.to(".p1", {
 				duration: 1.5,
 				text: "The bug hunt can be a fickle game, fixing one can bring another to fame,",
 			});
-			tl.to(".revealSectionOne2", {
+			tl.to(".p2", {
 				duration: 1.5,
 				text: "As the codebase shifts and grows, new bugs transpire from deep below.",
 			});
-			tl.to(".revealSectionOne3", {
+			tl.to(".p3", {
 				duration: 1.5,
 				text: "For bugs will always have their way, still we push our code to prod anyway",
 			});
@@ -41,7 +39,7 @@ function DescSectionOne() {
 				duration: 0.5,
 				width: "80%",
 			});
-			tl.to(".revealSectionOne4", {
+			tl.to(".p4", {
 				duration: 0.5,
 				text: "(wait what??)",
 			});
@@ -52,7 +50,7 @@ function DescSectionOne() {
 	return (
 		<section
 			ref={refS1}
-			className="h-screen  "
+			className="h-screen relative"
 			id="s3"
 			style={{
 				backgroundImage: `url(${backgroundOrange})`,
@@ -60,19 +58,23 @@ function DescSectionOne() {
 				backgroundPosition: "80% 0%",
 			}}>
 			{/* <img src={backgroundOrange} className={"absolute h-screen w-screen object-fill"}  /> */}
-			<div style={{ backgroundColor: ducks[activeDuck].primaryClr }} className="absolute h-[12em] w-[18em] sm:h-[14.5em] sm:w-[24em] left-[10vw] bottom-[100px] *:py-2 pl-2 *:w-full ">
+			<div
+				style={{ backgroundColor: ducks[activeDuck].primaryClr }}
+				className="absolute h-[12em] w-[18em] sm:h-[14.5em] sm:w-[24em] left-[10vw] bottom-[100px] *:py-2 pl-2 *:w-full z-20 ">
 				<P>
-					<span className="revealSectionOne1"></span>
+					<span className="p1"></span>
 				</P>
 				<P style={"mr-8"}>
-					<span className="revealSectionOne2"></span>
+					<span className="p2"></span>
 				</P>
-				<P style={"relative"}>
-					<span className="revealSectionOne3 relative"></span>
+				<div className="relative">
+					<P>
+						<span className="p3 relative"></span>
+					</P>
 					<div style={{ backgroundColor: ducks[activeDuck].secondaryClr }} className=" revealScratch absolute w-0 h-[1px] bottom-4 sm:bottom-5 right-4"></div>
-				</P>
+				</div>
 				<P>
-					<span className="revealSectionOne4"></span>
+					<span className="p4"></span>
 				</P>
 			</div>
 		</section>
